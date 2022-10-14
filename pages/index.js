c33eac6659c08b63e42d1b3b1b6e981e2370f3a3
Steven Barkley <steven.barkley@outlook.com>
2022-10-14 18:45:13 -0400

    Added Banner to Pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Banner from '../components/banner.js'
 
 export default function Home() {
   console.log( "styles", styles )
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <main className={ styles.main }>
         <h1 className={ styles.title }>Coffee Connoisseur</h1>
+        <Banner />
       </main>
 
     </div>
